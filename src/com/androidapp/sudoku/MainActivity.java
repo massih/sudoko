89e67aa8535f27caa5cfa29fe37d9f85ae4a37d7
Massih M <massih.z@gmail.com>
2013-06-20 16:27:46 +0200

    add a button to main activity which start the single_page_activity

--- src/com/androidapp/sudoku/MainActivity.java
+++ src/com/androidapp/sudoku/MainActivity.java
@@ -1,8 +1,10 @@
 package com.androidapp.sudoku;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void showSingleGameActivity(View view) {
+		Intent intent = new Intent(this, SingleGamePageActivity.class);
+		startActivity(intent);
 	}
 
 }
